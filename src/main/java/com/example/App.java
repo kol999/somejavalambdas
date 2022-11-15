2e9389094d6be6232ed9e2aee4a649174c6e5848
kol <kol@work.com>
2022-11-15 17:32:09 +0000

    Added a couple of print statements.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,12 +3,19 @@ package com.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map; 
+import java.io.IOException;
 /**
  * Handler 
  */
 public class App implements RequestHandler<Map<String,String>,String>{
     public String handleRequest(Map<String,String> event, Context context) {
-        String response = "200 OK";
-        return response;
+        try{ 
+            System.out.println("Hello from the Lamba function");
+            System.out.println(event);
+            String response = "200 OK";
+            return response;
+        }catch(Exception e){
+            return e.getMessage(); 
+        }
     }
 }
