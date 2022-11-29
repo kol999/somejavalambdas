9407bac05d5c56a18103b488695472a3458b0256
kol <kol@work.com>
2022-11-29 19:41:05 +0000

    Adding logging.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # somejavalambdas
-Some various lambda functions for Java. 
+
+To deploy the lambda functions run:
+
+mvn clean package
+
+aws lambda update-function-code --function-name HelloJava --zip-file file://demo-1.0-SNAPSHOT.jar 
+
+Some various lambda functions for Java. By branch:
+
+main branch: 
+	The main is a simple java handler that will return a pojo.   
+
+branch 
+git switch branch1 
+
+git switch branch2 
+
+
+
+
+
+
--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,15 +3,28 @@ package com.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map; 
-import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
- * Handler 
+ * Handler
+ * A very straightforward handler that accepts a Map and returns a String.
+ * 
  */
 public class App implements RequestHandler<Map<String,String>,String>{
+
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public String handleRequest(Map<String,String> event, Context context) {
+        logger.info("EVENT:" + gson.toJson(event));
         try{ 
             System.out.println("Hello from the Lamba function");
-            System.out.println(event);
             String response = "200 OK";
             return response;
         }catch(Exception e){
